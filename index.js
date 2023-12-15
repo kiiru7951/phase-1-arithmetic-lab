a28da0b9584a2e90b38a2e2e16a1ca4cd5aef7fb
Allan <allan.kariuki@student.moringaschool.com>
2023-12-15 23:04:35 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random;
+random = Number.parseInt(Math.floor(Math.random()*10)+1, 10);
+
+let mod;
+let num4 = 20
+let num3 = 5
+mod = num4 / num3
+
+let max;
+max = Math.max(15,16,17,18,19,20)
